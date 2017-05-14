912eb0543b2786be7802b16a8b4dd276044f7a77
Lucas Ventura <lnventura@hotmail.com>
2017-05-14 16:08:31 -0300

    testing if redis trying to connect on production or develop environment

--- app/redisconnection.js
+++ app/redisconnection.js
@@ -2,13 +2,15 @@ var redis = require('redis');
 var redisClient = null;
 
 if (process.env.REDISTOGO_URL) {
+    console.log('redis production environment activate');
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     redisClient = require("redis").createClient(rtg.port, rtg.hostname);
     redisClient.auth(rtg.auth.split(":")[1]);
 } else {
-    //var config = require('../config/config');
-    //redisClient = redis.createClient(config.redisConf);
-    //redisClient.auth(config.redisConf.password);
+    console.log('redis developer environment activate');
+    var config = require('../config/config');
+    redisClient = redis.createClient(config.redisConf);
+    redisClient.auth(config.redisConf.password);
 }
 
 redisClient.on('connect', function() {
