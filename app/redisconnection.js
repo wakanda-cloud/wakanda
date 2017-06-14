3d736550dfeec71eb3e45492ce6b2a6d8adfecbc
Lucas Ventura <lnventura@hotmail.com>
2017-06-13 21:57:10 -0300

    Adding capability to work with Heroku Redis too
    Now supports Heroku Redis and Redis Cloud and Another Remote Redis

--- app/redisconnection.js
+++ app/redisconnection.js
@@ -3,6 +3,8 @@ var redisClient = null;
 
 if (process.env.REDISCLOUD_URL) {
     redisClient = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+} else if (process.env.REDIS_URL) {
+    redisClient = redis.createClient(process.env.REDIS_URL, {no_ready_check: true});
 } else {
     var config = require('../config/config');
     redisClient = redis.createClient(config.redisConf);
