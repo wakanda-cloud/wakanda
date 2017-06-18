0e6a9d37ad8f53be4163a867eea3047338bef1b6
Ventura <lnventura@hotmail.com>
2017-06-18 19:12:10 -0300

    readding utf8 toString decrypt

--- app/securityService.js
+++ app/securityService.js
@@ -10,7 +10,7 @@ securityService.decryptJSON = function(text) {
         mode: CryptoJS.mode.CTR
     });
 
-    return JSON.parse(bytes.toString());
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 };
 
 module.exports = securityService;
